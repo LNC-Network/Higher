79f5bc781fbba192b1aa4c2cd38c97c2a86ce66f
Jit Debnath <jit.nathdeb@gmail.com>
2025-04-05 22:03:16 +0530

    Refactor Markdown editor: replace Milkdown with ReactMarkdown, enhance markdown insertion functionality, and improve rendering components; update page structure and dependencies.

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -6,12 +6,8 @@ import GenerateButton from "./GenerateButton";
 import PreferenceButton from "./PreferenceButton";
 import SaveButton from "./SaveButton";
 import ConnectWallet from "@/components/ui/ConnectWallet";
-import { useRef } from "react";
-import { useMessage } from "@/context/GeminiOutput";
-import { MilkdownEditor } from "./editor";
+import MarkdownEditor from "./editor";
 const page = () => {
-  const { message, setMessage } = useMessage("");
-  const sourceRef = useRef<HTMLDivElement>(null);
   return (
     <div className="flex-1 overflow-auto h-screen">
       <GeminiOutputContextProvider>
@@ -20,7 +16,7 @@ const page = () => {
             {/* Navbar */}
             <nav className="flex justify-center bg-slate-900 w-full my-4 px-2">
               <div className="w-full flex justify-between items-center mx-auto">
-                <h1 className="bg-gradient-to-r from-[#bc00ff] to-[#00f3ff] bg-clip-text text-transparent text-3xl leading-none pb-1">Higher</h1>
+                <h1 className="bg-gradient-to-r from-[#bc00ff] to-[#00f3ff] bg-clip-text text-transparent text-3xl leading-none pb-1">Higher Writer</h1>
                 <div className="flex gap-2">
                   <GenerateButton />
                   <PreferenceButton />
@@ -31,9 +27,8 @@ const page = () => {
             </nav>
 
             {/* Markdown Editor */}
-            <div ref={sourceRef} className="w-full flex-1 h-[50vh] rounded-2xl overflow-hidden shadow-[0_0_40px_rgba(0,0,0,0.3)] border border-white/10 bg-slate-800">
-              <MilkdownEditor />
-            </div>
+
+            <MarkdownEditor />
           </div>
         </PreferenceContextProvider>
       </GeminiOutputContextProvider>
