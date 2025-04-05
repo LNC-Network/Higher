bd16a1d1636447393d7454e0cc73326b0853715b
Jit Debnath <jit.nathdeb@gmail.com>
2025-04-05 22:06:37 +0530

    Add ESLint configuration to next.config.ts to ignore builds

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
+const nextConfig: import("next").NextConfig = {
   images: {
     domains: ["oagsprvqqc.ufs.sh"],
   },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
