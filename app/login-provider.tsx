8e6e574c683d8cd9b3225d9c8597dad9e09f3fa9
Jit Debnath <jit.nathdeb@gmail.com>
2025-09-19 22:07:24 +0530

    auth added

--- app/login-provider.tsx
+++ app/login-provider.tsx
@@ -1,7 +1,20 @@
-"use client";
+'use client'
 
-import { SessionProvider } from "next-auth/react";
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { createBrowserClient } from '@supabase/ssr'
+import { useState } from 'react'
 
-export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [supabaseClient] = useState(() =>
+    createBrowserClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+  )
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  )
 }
